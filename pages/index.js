5b4a0986b2e82184d0b833892574c41c26ed0a53
Mike Fleming <mfleming@atlassian.com>
2020-08-30 13:07:36 -0500

    feat(prettier): adds prettier

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,28 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 import reducer, {
   USER_MOVE,
   RESET_GRID,
   RESIZE_GRID,
-  PROCESS_BOARD
-} from '../state/reducer'
-import styles from '../styles/Home.module.css'
+  PROCESS_BOARD,
+} from "../state/reducer";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  const [state, dispatch] = reducer()
+  const [state, dispatch] = reducer();
   const cellStyleMapping = {
-    '': '',
-    'X': styles.x,
-    '0': styles.o
-  }
+    "": "",
+    X: styles.x,
+    0: styles.o,
+  };
   const handleCellClick = (i, j) => () => {
     dispatch({
       type: USER_MOVE,
-      payload: { coordinates: [i, j] }
-    })
+      payload: { coordinates: [i, j] },
+    });
 
-    dispatch({ type: PROCESS_BOARD })
-  }
+    dispatch({ type: PROCESS_BOARD });
+  };
 
   return (
     <div className={styles.container}>
@@ -32,44 +32,51 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          T - T - T
-        </h1>
+        <h1 className={styles.title}>T - T - T</h1>
         <div className={styles.scoreboard}>
           {state.winner && <span>{`${state.winner} wins!`}</span>}
           {state.draw && <span>Draw 🤬</span>}
         </div>
         <div className={styles.grid}>
           {state.grid.map((row, i) => (
-            <div className={styles.row} key={i}>{row.map((cellValue, j) => (
-              <div
-                className={styles.cell}
-                key={j}
-                {...(cellValue === '' && { onClick: handleCellClick(i, j) })}
-              >
-                <div className={cellStyleMapping[cellValue]}/>
-              </div>
-            ))}</div>
+            <div className={styles.row} key={i}>
+              {row.map((cellValue, j) => (
+                <div
+                  className={styles.cell}
+                  key={j}
+                  {...(cellValue === "" && { onClick: handleCellClick(i, j) })}
+                >
+                  <div className={cellStyleMapping[cellValue]} />
+                </div>
+              ))}
+            </div>
           ))}
         </div>
 
         <div className={styles.settings}>
-            <input
-              type="range" 
-              disabled={state.movesMade > 0}
-              onChange={({ target: { value } }) => dispatch({
+          <input
+            type="range"
+            disabled={state.movesMade > 0}
+            onChange={({ target: { value } }) =>
+              dispatch({
                 type: RESIZE_GRID,
-                payload: { newSize: parseInt(value) }
-              })}
-              value={state.gridSize}
-              min={state.minSize}
-              max={state.maxSize}
-            />
-            {state.winner && (
-              <div className={styles.reset} onClick={() => dispatch({ type: RESET_GRID })}>Reset?</div>
-            )}
+                payload: { newSize: parseInt(value) },
+              })
+            }
+            value={state.gridSize}
+            min={state.minSize}
+            max={state.maxSize}
+          />
+          {state.winner && (
+            <div
+              className={styles.reset}
+              onClick={() => dispatch({ type: RESET_GRID })}
+            >
+              Reset?
+            </div>
+          )}
         </div>
       </main>
     </div>
-  )
+  );
 }
--- state/reducer.js
+++ state/reducer.js
@@ -1,140 +1,146 @@
-import { useReducer, useEffect, useRef, useMemo } from 'react'
+import { useReducer, useEffect, useRef, useMemo } from "react";
 
-export const USER_MOVE = 'user_move'
-export const RESET_GRID = 'reset_grid'
-export const RESIZE_GRID = 'resize_grid'
-export const PROCESS_BOARD = 'process_board'
+export const USER_MOVE = "user_move";
+export const RESET_GRID = "reset_grid";
+export const RESIZE_GRID = "resize_grid";
+export const PROCESS_BOARD = "process_board";
 
-export default () => useReducerWithLogger(reducer, initialState)
+export default () => useReducerWithLogger(reducer, initialState);
 
 const initialState = {
-    grid: [
-      ['', '', ''],
-      ['', '', ''],
-      ['', '', '']
-    ],
-    currentUser: 'X',
-    movesMade: 0,
-    gridSize: 3,
-    winner: false,
-    draw: false,
-    maxSize: 13,
-    minSize: 1
-}
+  grid: [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ],
+  currentUser: "X",
+  movesMade: 0,
+  gridSize: 3,
+  winner: false,
+  draw: false,
+  maxSize: 13,
+  minSize: 1,
+};
 
 function reducer(state, action) {
   switch (action.type) {
     case PROCESS_BOARD:
-      const winner = checkForWinner(state) && state.currentUser
+      const winner = checkForWinner(state) && state.currentUser;
       return {
         ...state,
-        currentUser: state.currentUser === 'X' ? '0' : 'X',
+        currentUser: state.currentUser === "X" ? "0" : "X",
         winner,
-        draw: !winner && state.movesMade === Math.pow(state.gridSize, 2)
-      }
+        draw: !winner && state.movesMade === Math.pow(state.gridSize, 2),
+      };
     case RESIZE_GRID:
-      const { newSize } = action.payload
+      const { newSize } = action.payload;
       return {
         ...state,
         gridSize: newSize,
-        grid: new Array(newSize).fill(
-          new Array(newSize).fill('')
-        )
-      }
+        grid: new Array(newSize).fill(new Array(newSize).fill("")),
+      };
     case RESET_GRID:
-      return initialState
+      return initialState;
     case USER_MOVE:
       const {
-        coordinates: [userI, userJ]
-      } = action.payload
+        coordinates: [userI, userJ],
+      } = action.payload;
 
       return {
         ...state,
         movesMade: state.movesMade + 1,
-        grid: state.grid.map((row, i) => row.map((cell, j) => {
+        grid: state.grid.map((row, i) =>
+          row.map((cell, j) => {
             if (userI === i && userJ === j) {
-              return state.currentUser
+              return state.currentUser;
             }
-            return cell
-        }))
-      }
+            return cell;
+          })
+        ),
+      };
     default:
-      throw new Error('Action not supported.')
+      throw new Error("Action not supported.");
   }
 }
 
 function checkForWinner(state) {
-    return checkLeftAndRight() ||
-      checkUpAndDown() ||
-      checkUpLeftAndDownRight() ||
-      checkUpRightAndDownLeft()
-  
-    function checkUpAndDown() {
-      let winner = false
-  
-      for (let j = 0; j < state.gridSize && !winner; j++) {
-        const cells = []
-  
-        for (let i = 0; i < state.gridSize; i++) {
-          cells.push(state.grid[i][j])
-        }
-  
-        winner = cells.filter(cell => cell === state.currentUser).length === state.gridSize
+  return (
+    checkLeftAndRight() ||
+    checkUpAndDown() ||
+    checkUpLeftAndDownRight() ||
+    checkUpRightAndDownLeft()
+  );
+
+  function checkUpAndDown() {
+    let winner = false;
+
+    for (let j = 0; j < state.gridSize && !winner; j++) {
+      const cells = [];
+
+      for (let i = 0; i < state.gridSize; i++) {
+        cells.push(state.grid[i][j]);
       }
-  
-      return winner
+
+      winner =
+        cells.filter((cell) => cell === state.currentUser).length ===
+        state.gridSize;
     }
-  
-    function checkLeftAndRight() {
-      let winner = false
-  
-      state.grid.forEach(row => {
-        if (!winner) {
-          winner = row.filter(cell => cell === state.currentUser).length === state.gridSize
-        }
-      })
-  
-      return winner
-    }
-  
-    function checkUpLeftAndDownRight() {
-      for (let i = 0; i < state.grid[0].length; i++) {
-        if (state.grid[i][i] !== state.currentUser) {
-          return false
-        }
-      }
-  
-      return true
-    }
-  
-    function checkUpRightAndDownLeft() {
-      for (let i = 0; i < state.grid[0].length; i++) {
-        if (state.grid[i][state.grid.length - 1 - i] !== state.currentUser) {
-          return false
-        }
-      }
-  
-      return true
-    }
+
+    return winner;
   }
+
+  function checkLeftAndRight() {
+    let winner = false;
+
+    state.grid.forEach((row) => {
+      if (!winner) {
+        winner =
+          row.filter((cell) => cell === state.currentUser).length ===
+          state.gridSize;
+      }
+    });
+
+    return winner;
+  }
+
+  function checkUpLeftAndDownRight() {
+    for (let i = 0; i < state.grid[0].length; i++) {
+      if (state.grid[i][i] !== state.currentUser) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  function checkUpRightAndDownLeft() {
+    for (let i = 0; i < state.grid[0].length; i++) {
+      if (state.grid[i][state.grid.length - 1 - i] !== state.currentUser) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
 
 function withLogger(dispatch) {
-    return (action) => {
-      console.log('Action type:', action.type)
-      return dispatch(action)
-    }
-  }
-  
+  return (action) => {
+    console.log("Action type:", action.type);
+    return dispatch(action);
+  };
+}
+
 function useReducerWithLogger(...args) {
-    let prevState = useRef(initialState)
-    const [state, dispatch] = useReducer(...args)
-    const dispatchWithLogger = useMemo(() => withLogger(dispatch), [dispatch])
+  let prevState = useRef(initialState);
+  const [state, dispatch] = useReducer(...args);
+  const dispatchWithLogger = useMemo(() => withLogger(dispatch), [dispatch]);
 
-    useEffect(() => {
-        console.log('Prev state:', prevState.current)
-        console.log('Next state:', state)
-        prevState.current = state
-    }, [state])
+  useEffect(() => {
+    console.log("Prev state:", prevState.current);
+    console.log("Next state:", state);
+    prevState.current = state;
+  }, [state]);
 
-    return [state, dispatchWithLogger]
+  return [state, dispatchWithLogger];
 }
